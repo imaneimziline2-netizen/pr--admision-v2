0b1f36b3f3c3fd71aa1c961eacadef403f4c2934
imane imziline <imaneimzline2@gmail.com>
2026-01-27 15:39:43 +0100

    obtenir le design précédent

---
+++ healthCare.js
@@ -0,0 +1,102 @@
+let form = document.getElementById("myform");
+let message = document.getElementById("message");
+let tablebody = document.getElementById("tablebody");
+
+const demandes = [];
+
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    let nome = document.getElementById("nome").value;
+    let prenome = document.getElementById("prenome").value;
+    let telephone = document.getElementById("telephon").value;
+    let email = document.getElementById("email").value;
+    let date = document.getElementById("date").value;
+    let motif = document.getElementById("motif").value;
+
+    let inputs = document.querySelectorAll("input");
+    let txter = document.querySelector("textarea");
+    let array = Array.from(inputs);
+    array.push(txter);
+
+    if (
+        nome === "" ||
+        prenome === "" ||
+        telephone === "" ||
+        email === "" ||
+        date === "" ||
+        motif === ""
+    ) {
+        message.innerText = "Vous de devez remplir toutes les champs";
+        message.style.color = "red";
+        message.style.fontWeight = "bold";
+        message.style.textAlign = "center";
+        controlMessage();
+        // hada dyal lon dyal input llhawya
+
+        for (let i = 0; i < array.length; i++) {
+            if (array[i].value === "") {
+                array[i].classList.add("roug");
+            } else {
+                array[i].classList.remove("roug");
+            }
+        }
+        return; //kathbs lina ila l9at chi unput khawya
+    }
+
+    let demande = {
+        nome: nome,
+        prenome: prenome,
+        telephone: telephone,
+        email: email,
+        date: date,
+        motif: motif,
+    };
+
+    demandes.push(demande);
+
+    romplirTableax();
+
+    message.innerText = "ajouter avec succée";
+    message.style.color = "green";
+    message.style.fontWeight = "bold";
+    message.style.textAlign = "center";
+    controlMessage();
+
+    form.reset();
+    for (let i = 0; i < array.length; i++) {
+        array[i].classList.remove("roug");
+    }
+});
+
+function romplirTableax() {
+    tablebody.innerHTML = ``;
+
+    demandes.forEach((obj, index) => {
+        let tr = document.createElement("tr");
+
+        tr.innerHTML = `
+ <td>${obj.nome}</td>
+ <td>${obj.prenome}</td>
+ <td>${obj.telephone}</td>
+ <td>${obj.email}</td>
+ <td>${obj.date}</td>
+ <td>${obj.motif}</td>
+ <td><button class="suprim" onclick="funcDelet(${index})" >🗑️supprimer</button></td>
+ `;
+        tablebody.appendChild(tr);
+    });
+}
+
+function funcDelet(index) {
+    demandes.splice(index, 1);
+    romplirTableax();
+}
+
+function controlMessage() {
+    message.classList.add("show-msg");
+
+    setTimeout(() => {
+        message.classList.remove("show-msg");
+    }, 3000);
+}
